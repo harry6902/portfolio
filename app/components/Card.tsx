7662f7000f699c565a9e8bc97e35e37346be95d5
harry6902 <manepallihari2002@gmail.com>
2025-01-26 01:45:07 +0530

    adding files

---
+++ app/components/Card.tsx
@@ -0,0 +1,46 @@
+import { div } from "framer-motion/client";
+import Link from "next/link";
+
+export function Card({heading,description,gitlink,hostlink,imgUrl}:{
+    heading:string,
+    description:string,
+    gitlink:string,
+    hostlink?:string,
+    imgUrl:string
+}){
+    return(
+        <div className="min-h-[30vh] w-[80vw] lg:w-[30vw] bg-gray-900 border border-gray-400 p-5 rounded-md relative" >
+            <div className="h-full w-full flex flex-col gap-1">
+            <h1 className="text-xl md:text-2xl font-bold h-8 mb-2">{heading}</h1>
+            <img src={imgUrl} alt="" className="w-[80vw] lg:w-[30vw] h-[28vh] object-cover" />
+            <p className="text-xs text-gray-300 text-center font-mono h-16 md:h-12">{description}</p>
+            <div className="flex items-center gap-5 mt-2">
+            <div className=" bg-gray-700 p-2 rounded-md cursor-pointer hover:bg-gray-600 ">
+             <Link href={gitlink} className="flex justify-center items-center gap-2" target="_blank">
+            <p>Code</p>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffffff" fill="none">
+    <path d="M11.0991 3.00012C7.45013 3.00669 5.53932 3.09629 4.31817 4.31764C3.00034 5.63568 3.00034 7.75704 3.00034 11.9997C3.00034 16.2424 3.00034 18.3638 4.31817 19.6818C5.63599 20.9999 7.75701 20.9999 11.9991 20.9999C16.241 20.9999 18.3621 20.9999 19.6799 19.6818C20.901 18.4605 20.9906 16.5493 20.9972 12.8998" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M20.556 3.49612L11.0487 13.0586M20.556 3.49612C20.062 3.00151 16.7343 3.04761 16.0308 3.05762M20.556 3.49612C21.05 3.99074 21.0039 7.32273 20.9939 8.02714" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+</svg>
+</Link>
+</div>
+
+{
+    hostlink && <div className=" bg-gray-700 p-2 rounded-md cursor-pointer hover:bg-gray-600">
+       <Link href={hostlink} className="flex justify-center items-center gap-2" target="_blank">
+        <p>View</p>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffffff" fill="none">
+    <path d="M11.0991 3.00012C7.45013 3.00669 5.53932 3.09629 4.31817 4.31764C3.00034 5.63568 3.00034 7.75704 3.00034 11.9997C3.00034 16.2424 3.00034 18.3638 4.31817 19.6818C5.63599 20.9999 7.75701 20.9999 11.9991 20.9999C16.241 20.9999 18.3621 20.9999 19.6799 19.6818C20.901 18.4605 20.9906 16.5493 20.9972 12.8998" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M20.556 3.49612L11.0487 13.0586M20.556 3.49612C20.062 3.00151 16.7343 3.04761 16.0308 3.05762M20.556 3.49612C21.05 3.99074 21.0039 7.32273 20.9939 8.02714" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+</svg>
+</Link>
+
+    </div>
+}
+            </div>
+
+            </div>
+        </div>
+    )
+
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,192 @@
+"use client"
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { Github, Linkedin,Mail, Twitter, ArrowDown, CaseUpper,FileText } from "lucide-react";
+import Link from "next/link";
+import { Card } from './components/Card';
 
 export default function Home() {
+  const skills = [
+    "HTML",
+    "CSS",
+    "Express",
+    "Node",
+    "React",
+    "NextJs",
+    "MongoDb",
+    "Postgres",
+    "WebSockets",
+    "TurboRepo",
+    "Github",
+    "Docker",
+    "Kubernetes",
+    "ArgoCD",
+    "Solana",
+    "Ethereum",
+    "SmartContracts"
+  ];
+  
+  const scrollToSection = () => {
+    document.getElementById('projects-section')!.scrollIntoView({ behavior: 'smooth' });
+  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+  <main className="min-h-screen overflow-x-hidden w-[100vw]">
+    <section>
+    <div className="bg-slate-900 relative">
+     <div className="h-screen">
+    <img
+            src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?auto=format&fit=crop&q=80"
+            alt="Background"
+            className="w-full h-full object-cover opacity-30"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    
     </div>
+    </div>
+    <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+
+    <div className="absolute top-0 text-white h-screen w-screen flex flex-col justify-center items-center">
+    
+      <h1 className="text-4xl md:text-6xl font-bold mb-6">Manepalli Hari</h1>
+      <p className="text-lg md:text-2xl text-muted-foreground mb-8 text-slate-400">Full Stack Development, Web3 and DevOps Enthusiast</p>
+
+      <div className="flex gap-3 md:gap-5 mb-8">
+        <Link href={"https://github.com/harry6902"} target="_blank">
+        <div className="cursor-pointer h-10 w-10 bg-black rounded-md flex items-center justify-center hover:bg-slate-900"><Github className="h-5 w-5" /></div>
+        </Link>
+        <Link href={"https://www.linkedin.com/in/manepalli-hari-55a189217/"} target="_blank">
+        <div className="cursor-pointer h-10 w-10 bg-black rounded-md flex items-center justify-center hover:bg-gray-900"><Linkedin className="h-5 w-5"/></div>
+        </Link>
+        <Link href={"mailto:manepallihari2002@gmail.com"}>
+        <div className="cursor-pointer h-10 w-10 bg-black rounded-md flex items-center justify-center hover:bg-gray-900"><Mail className="h-5 w-5" /></div>
+        </Link>
+        <Link href={"https://x.com/harry_codes"} target="_blank">
+        <div className="cursor-pointer h-10 w-10 bg-black rounded-md flex items-center justify-center hover:bg-gray-900"><Twitter className="h-5 w-5"/></div>
+        </Link>
+        <Link href={"Resume_Hari.pdf"} target="_blank">
+        <div className="cursor-pointer h-10 w-10 bg-black rounded-md flex items-center justify-center hover:bg-gray-900">
+    
+            <FileText className="h-5 w-5" />
+
+        </div>
+        </Link>
+
+      </div>
+    
+      <button onClick={scrollToSection} className="bg-slate-800 px-3 py-2 rounded-md font-semibold flex justify-center items-center gap-1">
+        <p>View Projects</p>
+        <ArrowDown className="h-5 w-5" />
+      </button>
+    
+      
+
+    </div>
+    </motion.div>
+    </section>
+
+
+       {/*Tech Skills*/}
+    <section className="min-h-[60vh] bg-black text-white w-full flex flex-col justify-center items-center">
+    <h2 className="text-3xl font-bold text-center mb-12 mt-10">Tech Stack</h2>
+    <div className="grid grid-cols-4 md:grid-cols-10 gap-y-2 md:gap-y-3 mb-10">
+
+      {skills.map((skill)=>(
+            <div key={skill} className="flex flex-col justify-center items-center text-white">
+        
+            <Image
+              src={`/stack/${skill}.svg`}
+              alt={skill}
+              width={24}
+              height={24}
+              className="w-7 h-7 md:h-10 md:w-10 text-white"
+            />
+                  <p className="text-xs md:text-sm text-gray-500 font-bold">{skill}</p>
+            
+                  </div>
+
+      ))}
+  
+        
+    </div>
+
+    </section>
+
+    {/*Projects  Section*/}
+    <section id="projects-section" className="min-h-[50vh] bg-slate-900  text-white w-full flex flex-col justify-center items-center pb-5">
+    <h2 className="text-3xl font-bold text-center mb-12 mt-10">Projects</h2>
+    <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-x-3 gap-y-3">
+    <Card 
+    heading={"Excalidraw"}
+    description={"A real-time collaborative whiteboard where users can draw various shapes, write text, and interact seamlessly with updates appearing instantly for all participants."}
+    gitlink={"https://github.com/harry6902/excalidraw"}
+    imgUrl={"/projects/excali.png"}
+
+    />
+    <Card
+   heading={"Real Time Chat Application"}
+   description={"A real-time chat application that connects users with similar interests and chat with not more than one people"}
+   gitlink={"https://github.com/harry6902/hari-omeglechat-frontend"}
+   hostlink={"https://hari-omeglechat-frontend.vercel.app/"}
+   imgUrl={"/projects/chat.png"}
+    />
+    <Card
+       heading={"Pitch Your StartUp"}
+       description={"A platform where users can pitch their startups, allowing users to showcase their ideas and let people know about it."}
+       gitlink={"https://github.com/harry6902/harry-directory"}
+       hostlink={"https://harry-directory.vercel.app/"}
+       imgUrl={"/projects/startup.png"}
+    />
+    <Card
+       heading={"Cloud Based Wallet"}
+       description={"A website where users can sign up to automatically create a Solana wallet, transfer SOL using another users public address, and check our wallet balance"}
+       gitlink={"https://github.com/harry6902/cloudwallet-backend"}
+       imgUrl={"/projects/wallet.png"}
+    />
+    <Card
+       heading={"Ethereum Bridge Contract"}
+       description={"Ethereum smart contract enabling token transfers between the Ethereum blockchain and other blockchains, with the ability to retrieve tokens when needed."}
+       gitlink={"https://github.com/harry6902/eth-bridges"}
+       imgUrl={"/projects/bridge.png"}
+    />
+    <div className="flex items-center justify-center">
+      <Link href={"https://github.com/harry6902"} className=" bg-gray-700 hover:bg-gray-600 w-60 rounded-md p-2 cursor-pointer text-center" target="_blank">
+        Visit GitHub for more Projects
+      </Link>
+
+    </div>
+    </div>
+    </section>
+    {/* ABout me */}
+
+    <section className="min-h-[50vh] bg-black text-white" >
+
+      <div className="flex flex-col md:flex-row w-full">
+        <div className="lg:w-[25vw] h-full flex flex-col justify-center items-center">
+
+          <img src="./myimg2.png" alt=""  className="h-32 w-32 rounded-full object-cover mt-10"/>
+          <h1 className=" italic font-bold text-xl md:text-3xl mt-2 md:mt-5">Manepalli Hari</h1>
+          <h2 className="texl-lg md:text-2xl">IIT Jodhpur CSE Graduate</h2>
+
+
+
+        </div>
+        <div className="lg:w-[75vh] flex flex-col items-center md:items-start justify-center md:ml-16">
+        <h1 className=" font-bold text-lg md:text-xl mt-2 md:mt-5">Get in touch</h1>
+        <p className="w-[70vw] md:w-[50vw] text-gray-400">I’m open to exciting projects and collaborations! Let’s bring your idea to life or solve your web development challenges—reach out anytime.</p>
+          
+          <p className="text-gray-400">Ph.No: +91-9352218875</p>
+          <p className="text-gray-400">📧 Email: <span className="text-blue-500">manepallihari2002@gmail.com</span></p>
+
+        </div>
+
+      </div>
+
+
+    </section>
+
+  </main>
   );
 }
